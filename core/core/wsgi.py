dfd7315f06fa9ef6789c77c6c6ce609afbf6c37e
Cuong <44018211+cuongnx3111999@users.noreply.github.com>
2022-11-24 14:16:45 +0700

    1

---
+++ core/core/wsgi.py
@@ -0,0 +1,25 @@
+"""
+WSGI config for core project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+import pathlib
+
+from django.core.wsgi import get_wsgi_application
+
+import dotenv
+
+CURRNT_DIR=pathlib.Path(__file__).resolve().parent
+BASE_DIR=CURRNT_DIR.parent
+ENV_FILE_PATH=BASE_DIR / ".env"
+
+dotenv.read_dotenv(str(ENV_FILE_PATH))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+application = get_wsgi_application()
